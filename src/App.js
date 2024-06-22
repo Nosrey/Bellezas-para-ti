912d3ffa2bbf72b727eb8568d0e1d51d530ddbc5
Nosrey <nosrey135@gmail.com>
2024-06-21 22:29:16 -0400

    homepage finalizada

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { ShopCategory } from './Pages/ShopCategory';
 import { Product } from './Pages/Product';
 import { Cart } from './Pages/Cart';
 import { LoginSignup } from './Pages/LoginSignup';
+import { Footer } from './Components/Footer/Footer';
 
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
           {/* ahora /login para loginSignup */}
           <Route path='/login' element={<LoginSignup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
